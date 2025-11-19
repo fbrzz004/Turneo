20f4697676ff49cea3de1e1a5ec73e993fc995ec
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2025-11-19 14:46:01 -0500

    error de intl

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+// 1. Importaciones necesarias para el idioma
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'login/inicio_screen.dart';
 
-void main() {
+// 2. Convertimos el main a async para esperar la inicialización
+void main() async {
+  // 3. Aseguramos que los widgets estén listos
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 4. Inicializamos el formato de fechas para Español ('es_ES')
+  // Esto soluciona el error "Locale data has not been initialized"
+  await initializeDateFormatting('es_ES', null);
+
   runApp(const TurneoApp());
 }
 
@@ -13,6 +25,17 @@ class TurneoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Turneo',
       debugShowCheckedModeBanner: false,
+
+      // 5. Configuramos el idioma de los widgets de Flutter (como el DatePicker)
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', 'ES'), // Definimos Español como soportado
+      ],
+
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.light,
